7802f2311a6d5420211aa2c3b1299b616dc305bb
nafisa <nafisasharif91@hotmail.com>
2016-03-08 17:30:48 +0000

    Updated with name changes

--- regression_1D_example.py
+++ regression_1D_example.py
@@ -1,6 +1,6 @@
 import numpy as np
 import kernels
-import regression
+import regression_1D
 
 # Produce input range and true function values
 # Our model does not know the true function values, but we will plot them to get an idea of how the model is performing
@@ -10,16 +10,16 @@ true_f = np.sin(0.9*input_range)
 
 # Calculate noise of function values and noise variance
 # Noise variance is a parameter of the regression model
-noise = np.vstack(0.05*np.random.rand(50, 1))
+noise = np.vstack(np.random.randn(50, 1))
 noise_variance = np.var(noise)
 
 # Generate test values (data)
 # The posterior is generated using the test 
 X_train = np.vstack(5*np.random.rand(3, 1))
-train_noise = np.vstack(0.05*np.random.rand(3, 1))
+train_noise = np.vstack(np.random.randn(3, 1))
 Y_train = np.sin(0.9*X_train) + train_noise
 
 kern = kernels.RBF()
-m = regression.Regression(input_range, true_f, X_train, Y_train, noise_variance, kernel=kern) # Optional: kernel, normalize
+m = regression_1D.Regression(input_range, true_f, X_train, Y_train, noise_variance, kernel=kern) # Optional: kernel, normalize
 m.plot_prior()
 m.plot_posterior(X_train, Y_train)
