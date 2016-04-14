313d96ce1bfcadc5ad6dc5588eec4e22552dc41c
nafisa <nafisasharif91@hotmail.com>
2016-04-14 16:50:33 +0100

    Added more training points

--- regression_1D_example.py
+++ regression_1D_example.py
@@ -8,18 +8,13 @@ import regression_1D
 input_range = np.vstack(np.linspace(0, 5, 50))
 true_f = np.sin(0.9*input_range)
 
-# Calculate noise of function values and noise variance
-# Noise variance is a parameter of the regression model
-noise = np.vstack(0.4*np.random.randn(50, 1))
-noise_variance = np.var(noise)
-
-# Generate test values (data)
-# The posterior is generated using the test 
-X_train = np.vstack(5*np.random.rand(5, 1))
-train_noise = np.vstack(0.05*np.random.randn(5, 1))
+# Generate training values (data)
+# The posterior is generated using these values 
+X_train = np.vstack(3*np.random.rand(40, 1))
+train_noise = np.vstack(0.05*np.random.randn(40, 1))
 Y_train = np.sin(0.9*X_train) + train_noise
 
-kern = kernels.RBF()
+kern = kernels.RBF(lengthscale=0.5, noise_var=0.5)
 m = regression_1D.Regression(input_range, true_f, X_train, Y_train, kernel=kern) # Optional: kernel, normalize
 m.plot_prior()
 m.plot_posterior(X_train, Y_train)
