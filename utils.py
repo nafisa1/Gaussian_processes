c7198e424422bca881166d050575b0aa147d2673
nafisa <nafisasharif91@hotmail.com>
2016-05-24 17:59:45 +0100

    Rewrote latin hypercube code to increase final number of random choices

--- utils.py
+++ utils.py
@@ -3,22 +3,21 @@ import numpy as np
 # Latin hypercube sampling
 
 class LHS(object):
-	def __init__(self, parameters=3, divisions=6, lower=0.5, upper=2.5):
+	def __init__(self, parameters=3, n_choices=15, lower=0.5, upper=2.5, divisions=6):
 		self.parameters = parameters
 		self.divisions = divisions
 		self.lower = lower
 		self.upper = upper
 
 		a = np.linspace(lower,upper,divisions)
+		options = len(a)
 
-		div_list = []
+		full = np.zeros(((options**3),parameters))
 
-		for i in xrange(parameters):
-			div_list.append(np.random.permutation(a))
-	
-		div_list = np.array((div_list))
+		full[:,0] = np.concatenate(((options**2)*[a[0]], (options**2)*[a[1]], (options**2)*[a[2]], (options**2)*[a[3]], (options**2)*[a[4]], (options**2)*[a[5]]))
+		full[:,1] = np.concatenate(options*[np.concatenate((options*[a[0]], options*[a[1]], options*[a[2]], options*[a[3]], options*[a[4]], options*[a[5]]))])
+		full[:,2] = np.concatenate((options**2)*[a])
 
-		combinations = np.dstack(div_list)
-		self.combinations = combinations[0]
+		self.combinations = full[np.random.randint(full.shape[0], size=n_choices),:]
 
 	
