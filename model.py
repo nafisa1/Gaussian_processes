106f6a9bfec3b3324641f2bf36dc52209b4b2bd7
nafisa <nafisasharif91@hotmail.com>
2016-07-19 17:01:09 +0100

    Added functions incorporating regression, optimisation, and dummy functions for prior mean correction and classification using a threshold

--- model.py
+++ model.py
@@ -1,24 +1,25 @@
 import numpy as np
-from utils import normalize_centre, centre, LHS
+import utils
 import GPy
-import regression
 
-class Model(object):
+class Model(Regression):
 
-	def __init__(self, X, Y, latent_dim, have_Ytest=True, shuffle=True, split_train=0.8, kernel=None, prior=None):
+	def __init__(self, X, Y, latent_dim, have_Ytest=True, shuffle=True, split_train=0.8, kernel=None, prior=None, acq_func=None):
 		self.X = X
 		self.Y = Y
 		self.kernel = kernel
 		self.have_Ytest = have_Ytest
 		self.split_train = split_train
 		self.prior = prior
+		self.acq_func = acq_func
 
 		# Sanity check
 		if len(Y.shape) != 2:
 			Y = Y.reshape(-1,1)
 	
 		# Normalise and centre X, perform PCA
-		X = normalize_centre(X)
+		X = utils.remove_zero_cols(X)
+		X = utils.normalize_centre(X)
 		X_pcs, W = GPy.util.linalg.pca(X, latent_dim)
 		jitter = 0.05*np.random.rand((X_pcs.shape[0]), (X_pcs.shape[1]))
 		jitter -= 0.025
@@ -42,8 +43,8 @@ class Model(object):
 			Ytest = Y[(split_train*X.shape[0]):,:]
 
 			# Centre Y
-			self.Ytrain = centre(Ytrain)
-			self.Ytest = centre(Ytrain, Ytest)
+			self.Ytrain = utils.centre(Ytrain)
+			self.Ytest = utils.centre(Ytrain, Ytest)
 
 		else:
 
@@ -52,7 +53,7 @@ class Model(object):
 			self.Xtest = X_pcs[Y.shape[0]:,:]
 
 			# Centre Y
-			self.Ytrain = centre(Y)
+			self.Ytrain = utils.centre(Y)
 
 		if prior is not None:
 			prior = prior.reshape(-1,1)
@@ -64,17 +65,49 @@ class Model(object):
 			self.prior_test = prior[self.Xtrain.shape[0]:,:]
 
 	def hyperparameters(self):
-		lat_hyp = LHS(self.kernel)
+		lat_hyp = utils.LHS(self.kernel)
 
 		Xtrain = self.Xtrain[:((self.Xtrain.shape[0])*0.8),:]
 		Xtest = self.Xtrain[((self.Xtrain.shape[0])*0.8):,:]
 		Ytrain = self.Ytrain[:((self.Ytrain.shape[0])*0.8),:]
 		Ytest = self.Ytrain[((self.Ytrain.shape[0])*0.8):,:]
-		print Xtrain.shape, Xtest.shape, Ytrain.shape
+
 		self.kernel.lengthscale, self.kernel.sig_var, self.kernel.noise_var = lat_hyp.compute(Xtest, Xtrain, Ytrain, Ytest)
+
+	def regression(self):
+		import regression
+
+		if self.have_Ytest == True:
+			regress = regression.Regression(self.Xtest, self.Xtrain, self.Ytrain, kernel=self.kernel, Ytest=self.Ytest)
+
+		else:
+			regress = regression.Regression(self.Xtest, self.Xtrain, self.Ytrain, kernel=self.kernel, Ytest=None)
+
+		return regress
+
+	def optimization(self):
+
+		new_x, ind = self.acq_func.compute(self.Xtest, self.Xtrain, self.Ytrain, self.kernel)
+
+		new_obs = self.Ytest[ind]		
+
+		self.Xtrain = np.vstack((self.Xtrain, new_x))
+		self.Ytrain = np.vstack((self.Ytrain, new_obs))
+
+		self.Xtest = np.delete(self.Xtest, ind, axis=0)
+		self.Ytest = np.delete(self.Ytest, ind, axis=0)
+
+		return new_x
 
 	# This is done after regression etc			
 	def correction(self):
+		assert prior is not None, "Posterior mean correction is not required for a zero mean prior."
 		# add test prior mean to posterior mean
 		self.Ytest = self.Ytest + self.prior_test
 		# will also have to apply correction to random draws
+
+	def classify(self, threshold):
+		# separate into compounds below and above threshold
+		assert have_Ytest == True, "Experimental output values for the test set are required to calculate ROC plot."
+		# using real Ytest values, calculate true positives, false positives, true negatives, false negatives
+		# ROC plot (add function to plotting module)
