fc1c96c99b4a94598069ad342d6c1416d238b38c
nafisa <nafisasharif91@hotmail.com>
2016-12-13 11:58:24 +0000

    Added function to remove selected datapoints and shuffle function

--- utils.py
+++ utils.py
@@ -1,49 +1,89 @@
 import numpy as np
 
-def remove_zero_cols(A):
+def remove_zero_cols(A, B=None):
         # Calculate number of observations, N
 	N = A.shape[0]
 
 	# Remove columns of zeros
 	zeros = np.zeros((1,N))[0]
-	transpose = A.T
-
+	A_transpose = A.T
 	non_zero_cols = []
-	for row in transpose:
-		if np.array_equal(row, zeros) == False:
-       			non_zero_cols.append(row)
-       
-	non_zero_cols = np.array(non_zero_cols)
-	A = non_zero_cols.T
-		
-	return A
 
-def remove_identical(A):
+	if B==None:
+		for row in A_transpose:
+			if np.array_equal(row, zeros) == False:
+ 	      			non_zero_cols.append(row)
+       
+		non_zero_cols = np.array(non_zero_cols)
+		A = non_zero_cols.T
+
+		return A
+
+	else:
+		testT = B.T
+		selected_cols_test = []
+		
+		for row, rowtest in zip(A_transpose,testT):
+			if np.array_equal(row, zeros) == False and np.array_equal(rowtest, zeros) == False:
+				non_zero_cols.append(row)
+		        	selected_cols_test.append(rowtest)
+
+		non_zero_cols = np.array(non_zero_cols)
+		A = non_zero_cols.T
+
+		selected_cols_test = np.array(selected_cols_test)
+		B = selected_cols_test.T
+			
+		return A, B
+
+def remove_identical(A, B=None):
 	# To examine each column, take the transpose
 	transpose = A.T
 	n = transpose.shape[1]
-	B = []
+	non_identical = []
 
 	# Take each row of A.T and create array equal in length, where all elements are set to the first element of the original row
 	# Compare to identify rows where all elements are the same
-	for row in transpose:
-	        check = [] + n*[row[0]]
-	        check = np.asarray(check)
-        
-	        if np.array_equal(row,check) == False:
-			B.append(np.ndarray.tolist(row))
+
+	if B==None:
+		for row in transpose:
+	        	check = [] + n*[row[0]]
+	        	check = np.asarray(check)
+
+    	        	if np.array_equal(row,check) == False:
+				non_identical.append(np.ndarray.tolist(row))
+
+		return np.asarray(non_identical).T
+	
+	else:
+		non_identB = []
+		Btranspose = B.T
+
+		for row, rowB in zip(transpose,Btranspose):
+			check = [] + n*[row[0]]
+	        	check = np.asarray(check)
+			check_test = [] + n*[rowB[0]]
+	        	check_test = np.asarray(check_test)
+
+    	        	if np.array_equal(row,check) == False and np.array_equal(rowB,check_test) == False:
+				non_identical.append(np.ndarray.tolist(row))
+				non_identB.append(np.ndarray.tolist(rowB))
             
-	return np.asarray(B).T
+		return np.asarray(non_identical).T, np.asarray(non_identB).T
 
 def normalize_centre(A, B=None):
 	# Get mean and standard deviation
 	A_mu = np.vstack(np.mean(A, axis=0))
 	A_sd = np.vstack(A.std(axis=0))
+#	print A_mu
+	np.set_printoptions(threshold=np.nan)
+#	print A_sd.shape
+#	print A.shape
 
 	# Centre and normalize array
 	if B is None:
 		A_centred = A.T - A_mu
-		Acent_normalized = A_centred/A_sd                       
+		Acent_normalized = A_centred/A_sd
 		final_array = Acent_normalized.T   
 
 	# Centre and normalize second array using mean and standard deviation of first array
@@ -56,7 +96,11 @@ def normalize_centre(A, B=None):
 
 def centre(A, B=None):
 	# Get mean
-	A_mu = np.vstack(np.mean(A, axis=0))
+	if A.shape[1] > 1:
+		A_mu = np.vstack(np.mean(A, axis=0))
+
+	else:
+		A_mu = np.mean(A, axis=0)
 
 	# Centre array
 	if B is None:
@@ -83,6 +127,52 @@ def get_SMILES(filename, n_lines):
 			if count == n_lines:
 				break
 	return names
+
+def remove_500s(X, Y, smiles):
+	newX = []
+	newY = []
+	newSmiles = []
+
+	for Xrow, Ynum, smile in zip(X, Y, smiles):
+		if Ynum != 50000:
+			newX.append(Xrow)
+			newY.append(Ynum)
+			newSmiles.append(smile)
+            
+	return np.asarray(newX), np.asarray(newY), newSmiles
+
+
+def shuffle(X, Y, split=0.8, smiles=None, prior=None):
+	p = np.random.permutation(X.shape[0])
+	X = X[p]
+	Y = Y[p]
+	
+	if smiles is not None:
+		smiles = [smiles[i] for i in p]
+		smiles_train = smiles[:int(round(split*X.shape[0]))]
+		smiles_test = smiles[int(round(split*X.shape[0])):]
+	if prior is not None:
+		prior = prior[p]
+		prior_train = prior[:(split*X.shape[0]),:]
+		prior_test = prior[(split*X.shape[0]):,:]
+					
+	
+	Xtrain = X[:int(round(split*X.shape[0])),:]
+	Ytrain = Y[:int(round(split*X.shape[0]))]
+	Xtest = X[int(round(split*X.shape[0])):,:]
+	Ytest = Y[int(round(split*X.shape[0])):]
+	
+	if smiles is not None and prior is not None:
+		return Xtrain, Xtest, Ytrain, Ytest, smiles_train, smiles_test, prior_train, prior_test
+
+	elif smiles is not None and prior is None:
+		return Xtrain, Xtest, Ytrain, Ytest, smiles_train, smiles_test
+
+	elif smiles is None and prior is not None:
+		return Xtrain, Xtest, Ytrain, Ytest, prior_train, prior_test
+
+	else:
+		return Xtrain, Xtest, Ytrain, Ytest
 
 # Latin hypercube sampling
 
