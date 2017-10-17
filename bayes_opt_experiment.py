42b79dec5e0b7d240406ea116a201ccc226f6904
nafisa1 <nafisasharif91@hotmail.com>
2017-10-17 14:19:00 +0100

    Added end_train variable for more flexibility with training set

--- bayes_opt_experiment.py
+++ bayes_opt_experiment.py
@@ -3,9 +3,9 @@ import kernels
 import acquisition
 import run_experiment
 
-filename = 'datasets/EVOTEC_OC_DATASET_20170328.xlsx'
-id_name = 'EVOTEC_COMPOUND_ID'
-output_name = 'ACTIVITY_1_IC50_NM_AVG'
+filename = 'datasets/EVOTEC_OT_DATASET_20170328.xlsx'
+id_name = 'EVOTEC_ID'#_COMPOUND_ID'
+output_name = 'ACTIVITY_1_IC50__UM'#_AVG'
 smiles_name = 'CD_SMILES'
 descriptor_names = ['CD_MOLWEIGHT','TPSA']#'LOGD','LOGP','TPSA']
 
@@ -16,7 +16,7 @@ smiles, pic50s, descriptors = example.get_data(filename, id_name, smiles_name, o
 training_size = 20
 test_size = 20
 k = kernels.Composite(kernels.RBF(),kernels.Matern())
-noise = 0.01
-acq_func = acquisition.Chronological()
+n = 0.01
+acq_func = acquisition.Random()
 
-modopt,modtest, r_sq = example.bayes_opt(training_size, test_size, k, acq_func, number_runs=None)
+modopt,modtest, r_sq = example.bayes_opt(training_size, test_size, k, acq_func, noise=n, end_train=None, number_runs=None)
