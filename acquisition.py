097bbff3c319de91a27b01a934b6248ac1e8a397
nafisa1 <nafisasharif91@hotmail.com>
2018-10-26 18:32:49 +0100

    Removed dependency on regression module

--- acquisition.py
+++ acquisition.py
@@ -4,8 +4,10 @@ from regression import Regression
 from scipy.stats import norm
 
 class Random(object):
+	def __init__(self, abbreviation="rand"):
+		self.abbreviation = abbreviation
 
-	def compute(self, Xtest, Xtrain, Ytrain, kern, plot=False):
+	def compute(self, Xtest, Xtrain, Ytrain, sd, p_mean, plot=False):
 
 		if len(Xtest) != 2:
 			ind = np.random.randint(len(Xtest))
@@ -19,8 +21,10 @@ class Random(object):
 		return new_x, ind
 
 class Chronological(object):
+	def __init__(self, abbreviation="chron"):
+		self.abbreviation = abbreviation
 
-	def compute(self, Xtest, Xtrain, Ytrain, kern, plot=False):
+	def compute(self, Xtest, Xtrain, Ytrain, sd, p_mean, plot=False):
 		ind = 0
 		if len(Xtest) != 2:
 			new_x = Xtest[ind]
@@ -32,12 +36,10 @@ class Chronological(object):
 		return new_x, ind
 
 class Unc(object):
+	def __init__(self, abbreviation="unc"):
+		self.abbreviation = abbreviation
 
-	def compute(self, Xtest, Xtrain, Ytrain, kern, plot=False):
-		# Get posterior mean and standard deviation for test set
-		run = Regression(Xtest=Xtest, Xtrain=Xtrain, Ytrain=Ytrain, add_noise=0.0, kernel=kern, Ytest=None)
-		sd = run.post_s
-
+	def compute(self, Xtest, Xtrain, Ytrain, sd, p_mean, plot=False):
 		# Find maximum of acquisition function and corresponding test input
 		ind = np.argmax(sd)
 		if len(Xtest) != 2:
@@ -50,14 +52,12 @@ class Unc(object):
 		return new_x, ind
 
 class PI(object):
-	def __init__(self, eta=0.0):
+	def __init__(self, eta=0.0, abbreviation="pi"):
 		self.eta = eta
+		self.abbreviation = abbreviation
 
-	def compute(self, Xtest, Xtrain, Ytrain, kern, plot=False):
-		# Get posterior mean and standard deviation for test set
-		run = Regression(Xtest=Xtest, Xtrain=Xtrain, Ytrain=Ytrain, add_noise=0.0, kernel=kern, Ytest=None)
-		sd = run.post_s
-		p_mean = run.post_mean
+	def compute(self, Xtest, Xtrain, Ytrain, sd, p_mean, plot=False):
+		# sd p_mean 
 
 		# Find the minimum of the training values
 		f_max = np.amax(Ytrain)
@@ -81,26 +81,16 @@ class PI(object):
 			new_x = []
 			new_x.append(Xtest[0][ind]) # new numerical value(s)
 			new_x.append(Xtest[1][ind]) # new smiles
-		
-		# Take first principal component as X axis for plotting
-		#Xtest_axis = Xtest[:,0]	
-#		Xtrain_axis = Xtrain[:,0].reshape(-1,1)
-
-		# Plot posterior and acquisition function, showing preferred next observation
-		#if plot==True:		
-		#	plotting.plot_acq(Xtest_axis, acq, p_mean, sd, Ytest=None)
 
 		return new_x, ind
 		
 class EI(object):
-	def __init__(self, eta=0.0):
+	def __init__(self, eta=0.0, abbreviation="ei"):
 		self.eta = eta
+		self.abbreviation = abbreviation
 
-	def compute(self, Xtest, Xtrain, Ytrain, kern, plot=False):
-		# Get posterior mean and standard deviation for test set
-		run = Regression(Xtest=Xtest, Xtrain=Xtrain, Ytrain=Ytrain, add_noise=0.0, kernel=kern, Ytest=None)
-		sd = run.post_s
-		p_mean = run.post_mean
+	def compute(self, Xtest, Xtrain, Ytrain, sd, p_mean, plot=False):
+		# sd and p_mean
 
 		# Find the maximum of the training values
 		f_max = np.amax(Ytrain)
@@ -124,27 +114,16 @@ class EI(object):
 			new_x = []
 			new_x.append(Xtest[0][ind]) # new numerical value(s)
 			new_x.append(Xtest[1][ind]) # new smiles
-		
-		# Take first principal component as X axis for plotting
-#		Xtest_axis = Xtest[:,0]	
-#		Xtrain_axis = Xtrain[:,0].reshape(-1,1)
-
-#		if plot==True:
-		# Plot posterior and acquisition function, showing preferred next observation
-#			plotting.plot_acq(Xtest_axis, acq, p_mean, sd, Ytest=None)
 
 		return new_x, ind
 
 class UCB(object):
-	def __init__(self, kappa=0.2):
+	def __init__(self, kappa=0.2, abbreviation="ucb"):
 		self.kappa = kappa
+		self.abbreviation = abbreviation
 
-	def compute(self, Xtest, Xtrain, Ytrain, kern, plot=False):
-		# Get posterior mean and standard deviation for test set
-		run = Regression(Ytrain, Xtest=Xtest, Xtrain=Xtrain, add_noise=0.0, kernel=kern, Ytest=None)
-		sd = run.post_s
-		p_mean = run.post_mean
-
+	def compute(self, Xtest, Xtrain, Ytrain, sd, p_mean, plot=False):
+		# sd and p_mean
 		# Calculate acquisition function
 		acq = p_mean + (self.kappa*sd)
 
@@ -156,27 +135,17 @@ class UCB(object):
 			new_x = []
 			new_x.append(Xtest[0][ind]) # new numerical value(s)
 			new_x.append(Xtest[1][ind]) # new smiles
-			
-		# Take first principal component as X axis for plotting
-		#Xtest_axis = Xtest[:,0]	
-#		Xtrain_axis = Xtrain[:,0].reshape(-1,1)
-
-		#if plot==True:
-		# Plot posterior and acquisition function, showing preferred next observation
-		#	plotting.plot_acq(Xtest_axis, acq, p_mean, sd, Ytest=None)
 
 		return new_x, ind
 
 class LCB(object):
 
-	def __init__(self, kappa=0.2):
+	def __init__(self, kappa=0.2, abbreviation="lcb"):
 		self.kappa = kappa
+		self.abbreviation = abbreviation
 
-	def compute(self, Xtest, Xtrain, Ytrain, kern, plot=False):
-		# Get posterior mean and standard deviation for test set
-		run = Regression(Ytrain, Xtest=Xtest, Xtrain=Xtrain, add_noise=0.0, kernel=kern, Ytest=None)
-		sd = run.post_s
-		p_mean = run.post_mean
+	def compute(self, Xtest, Xtrain, Ytrain, sd, p_mean, plot=False):
+		# sd and p_mean
 
 		# Calculate acquisition function
 		acq = p_mean - (self.kappa*sd)
@@ -189,14 +158,6 @@ class LCB(object):
 			new_x = []
 			new_x.append(Xtest[0][ind]) # new numerical value(s)
 			new_x.append(Xtest[1][ind]) # new smiles
-		
-		# Take first principal component as X axis for plotting
-#		Xtest_axis = Xtest[:,0]	
-#		Xtrain_axis = Xtrain[:,0].reshape(-1,1)
-
-#		if plot==True:
-		# Plot posterior and acquisition function, showing preferred next observation
-#			plotting.plot_acq(Xtest_axis, acq, p_mean, sd, Ytest=None)
 
 		return new_x, ind
 
