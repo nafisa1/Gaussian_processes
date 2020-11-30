89c21ca3fb56e1aa9bc9b5926d52407f76ccab21
nafisa1 <nafisasharif91@hotmail.com>
2020-11-30 15:16:09 +0000

    Minor updates

--- bayes_opt_experiment.py
+++ bayes_opt_experiment.py
@@ -4,19 +4,24 @@ import acquisition
 import run_experiment
 
 filename = 'datasets/EVOTEC_OT_DATASET_20170328.xlsx'
-id_name = 'EVOTEC_ID'#_COMPOUND_ID'
+evotecIDs = 'EVOTEC_ID'#_COMPOUND_ID'
 output_name = 'ACTIVITY_1_IC50__UM'#_AVG'
 smiles_name = 'CD_SMILES'
 descriptor_names = ['CD_MOLWEIGHT','TPSA']#'LOGD','LOGP','TPSA']
 
 
 example = run_experiment.Experiment()
-smiles, pic50s, descriptors = example.get_data(filename, id_name, smiles_name, output_name, descriptor_names,upper_threshold=20000.0)
+smiles, pic50s, names, descriptors = example.get_data(filename, evotecIDs, smiles_name, output_name, descriptor_names,upper_threshold=20000.0)
 
 training_size = 20
-test_size = 20
+test_size = 80
 k = kernels.Composite(kernels.RBF(),kernels.Matern())
 n = 0.01
 acq_func = acquisition.Random()
 
-modopt,modtest, r_sq = example.bayes_opt(training_size, test_size, k, acq_func, noise=n, end_train=None, number_runs=None)
+modopt,modtest = example.bayes_opt(training_size, test_size, k, acq_func, noise=n, end_train=None, number_runs=5, print_interim=True)
+
+r = modtest.regression()
+r.plot_all()
+
+
