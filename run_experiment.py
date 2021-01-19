c82271b55a306218f9cfbc35e6b19ad8641d786f
nafisa1 <nafisasharif91@hotmail.com>
2021-01-19 19:49:18 +0000

    update bayes opt function

--- run_experiment.py
+++ run_experiment.py
@@ -29,11 +29,11 @@ class Experiment(object):
       
     print training_size, " initial training compounds"
     print test_size, " compounds in fixed test set"	
-    print number_runs, " compounds out of", start_test - training_size, " will be used for optimisation"
+    print number_runs, " compounds out of", self.start_test - training_size, " will be used for optimisation"
     
-    self.modopt = model.Model(n_kers=2, Xtrain=[descriptors[:training_size],smiles[:training_size]],Xtest=[descriptors[training_size:start_test],smiles[training_size:start_test]], Ytrain=output[:training_size], Ytest=output[training_size:start_test], kernel=ker)
+    self.modopt = model.Model(n_kers=2, Xtrain=[descriptors[:training_size],smiles[:training_size]],Xtest=[descriptors[training_size:self.start_test],smiles[training_size:self.start_test]], Ytrain=pic50s[:training_size], Ytest=pic50s[training_size:self.start_test], kernel=ker)
     self.modopt.kernel.noise_var = noise
-    self.modopt.hyperparameters(print_vals=True)
+#    self.modopt.hyperparameters(print_vals=True)
  
     # SET ACQUISITION FUNCTION   
     self.modopt.acq_func = acquisition_function
@@ -42,11 +42,11 @@ class Experiment(object):
     print "initial hps",self.modopt.kernel.kers[0].sig_var, self.modopt.kernel.kers[0].lengthscale, self.modopt.kernel.kers[1].sig_var, self.modopt.kernel.kers[1].lengthscale
    
     # CREATE MODEL TO MAKE PREDICTIONS ON TEST SET USING UPDATED TRAINING SET
-    self.modtest = model.Model(n_kers=2, Xtrain=self.modopt.Xtrain, Xtest=[descriptors[-test_size:],smiles[-test_size:]], Ytrain=self.modopt.Ytrain, Ytest=output[-test_size:], kernel=self.modopt.kernel) ## changed kernel from opt to train 
+    self.modtest = model.Model(n_kers=2, Xtrain=self.modopt.Xtrain, Xtest=[descriptors[-test_size:],smiles[-test_size:]], Ytrain=self.modopt.Ytrain, Ytest=pic50s[-test_size:], kernel=self.modopt.kernel) ## changed kernel from opt to train 
 
 
   def bayes_opt(self):
-  
+#    self.modopt.kernel.kers[0].sig_var, self.modopt.kernel.kers[0].lengthscale, self.modopt.kernel.kers[1].sig_var, self.modopt.kernel.kers[1].lengthscale = 5,0.8,5,0.8
 #    p = np.random.permutation(len(self.smiles))
 #    self.smiles, self.output, self.descriptors = [self.smiles[i] for i in p], self.output[p], [self.descriptors[i] for i in p]
 
@@ -54,21 +54,23 @@ class Experiment(object):
     # IF IT IS NOT DEFINED, SET END OF OPTIMISING SET TO START OF TEST SET
 
     # CREATE MODEL FOR SELECTING THE NEXT COMPOUND
-    modopt = model.Model(n_kers=2, Xtrain=[self.descriptors[:training_size],self.smiles[:training_size]],Xtest=[self.descriptors[training_size:start_test],self.smiles[training_size:start_test]], Ytrain=self.output[:training_size], Ytest=self.output[training_size:start_test], kernel=ker)
-    modopt.kernel.noise_var = noise
-    modopt.hyperparameters(print_vals=True)
- 
-    # SET ACQUISITION FUNCTION   
-    modopt.acq_func = acquisition_function
-#    modopt.mse_hyperparameters(print_vals=False)  
+#    modopt = model.Model(n_kers=2, Xtrain=[self.descriptors[:training_size],self.smiles[:training_size]],Xtest=[self.descriptors[training_size:start_test],self.smiles[training_size:start_test]], Ytrain=self.output[:training_size], Ytest=self.output[training_size:start_test], kernel=ker)
+#    modopt.kernel.noise_var = noise
+#    modopt.hyperparameters(print_vals=True)
 
-    print "initial hps",modopt.kernel.kers[0].sig_var, modopt.kernel.kers[0].lengthscale, modopt.kernel.kers[1].sig_var, modopt.kernel.kers[1].lengthscale
    
     # CREATE MODEL TO MAKE PREDICTIONS ON TEST SET USING UPDATED TRAINING SET
-    modtest = model.Model(n_kers=2, Xtrain=modopt.Xtrain, Xtest=[self.descriptors[start_test:],self.smiles[start_test:]], Ytrain=modopt.Ytrain, Ytest=self.output[start_test:], kernel=modopt.kernel) ## changed kernel from opt to train 
-    
+#    modtest = model.Model(n_kers=2, Xtrain=modopt.Xtrain, Xtest=[self.descriptors[start_test:],self.smiles[start_test:]], Ytrain=modopt.Ytrain, Ytest=self.output[start_test:], kernel=modopt.kernel) ## changed kernel from opt to train 
+
+#####################################################################
+
+#    self.modtest.Xtest = self.modtest.Xtrain
+#    self.modtest.Ytest = self.modtest.Ytrain   
+
+#####################################################################
+
     # PERFORM REGRESSION ON TEST SET USING INITIAL TRAINING SET
-    regtest = modtest.regression()
+    regtest = self.modtest.regression()
     print "Results on test set using initial training set"
     
     # KEEP TEST SET R SQUARED VALUES IN A LIST, THE FIRST VALUE IS THE R SQUARED VALUE FOR PREDICTIONS ON THE TEST SET USING THE ORIGINAL TRAINING SET BEFORE BAYESIAN OPTIMISATION 
@@ -77,20 +79,26 @@ class Experiment(object):
     print "Initial r squared",regtest.r_squared()
    
     # BEGIN BAYESIAN OPTIMISATION
-    for i in xrange(number_runs): 
-
+    for i in xrange(self.number_runs): 
+ #     self.modopt.hyperparameters(print_vals=False)
       # EVERY 10 RUNS, PRINT THE NUMBER OF THE RUN THE MODEL IS ON CURRENTLY
+
       if i%10 == 0:
         print "Iteration",i,"..."
         
       # RUN OPTIMISATION FUNCTION TO FIND THE NEW INPUT VALUE, AUTOMATICALLY ADDED TO THE TRAINING SET AND REMOVED FROM THE OPTIMISATION SET
 #######################?????????????????????
   
-      newx,newy = modopt.optimization()
+      newx,newy = self.modopt.optimization()
+      
+      
+      self.modopt.hyperparameters(print_vals=False)
+      self.modtest.kernel = self.modopt.kernel
       
       # RESET TRAINING SET OF THE TEST SET MODEL
-      modtest.Xtrain = modopt.Xtrain
-      modtest.Ytrain = modopt.Ytrain
+      self.modtest.Xtrain = self.modopt.Xtrain
+      self.modtest.Ytrain = self.modopt.Ytrain
+
         
       
       # RESET THE HYPERPARAMETERS OF THE TEST SET MODEL AS THE TRAINING SET HAS CHANGED
@@ -98,32 +106,35 @@ class Experiment(object):
 #      modopt.mse_hyperparameters(print_vals=False)
 
       # PERFORM REGRESSION ON THE TEST SET USING THE UPDATED TRAINING SET
-      regtest = modtest.regression()
+      regtest = self.modtest.regression()
       r_sq.append(regtest.r_squared())
       
       # PRINT R SQUARED FOR EACH RUN IF IT IS REQUESTED
-      if i != number_runs-1 and print_interim == True:
+      if i != self.number_runs-1 and self.print_interim == True:
         print "r squared",regtest.r_squared()
         
       # PRINT R SQUARED FOR FINAL RUN
-      elif i == number_runs-1:
+      elif i == self.number_runs-1:
         print "Results using final training set"
         print "r squared",regtest.r_squared()
         print "mse",regtest.mse()
         
 
-    print "training set mse: ", modopt.regression().mse()
-    print "test set mse: ", modtest.regression().mse()
-    modtest.regression().plot_by_index()
+#    print "training set mse: ", self.modopt.regression().mse()
+    print "test set mse: ", regtest.mse()
+    regtest.plot_by_index()
+    
 #    modopt.regression().plot_by_index()
         
     # ARRAY STARTING AT 0 FOR THE RUN BEFORE OPTIMISATION BEGINS
-    run = np.linspace(0,number_runs,num=number_runs+1)
+    run = np.linspace(0,self.number_runs,num=self.number_runs+1)
+    
+    print "final hps",self.modtest.kernel.kers[0].sig_var, self.modtest.kernel.kers[0].lengthscale, self.modtest.kernel.kers[1].sig_var, self.modtest.kernel.kers[1].lengthscale
       
     # SAVE R SQUARED FOR EACH RUN IN A TEXT FILE
-    np.savetxt("/home/nafisa/Dropbox/DPhil/Gaussian_processes/results/rsq_" + acquisition_function.abbreviation + "_tr" + str(training_size) + "_te" + str(test_size) + "_runs" + str(number_runs) + ".txt", np.c_[run,r_sq], fmt='%i	%f')
+#    np.savetxt("/home/nafisa/Dropbox/DPhil/Gaussian_processes/results/rsq_" + acquisition_function.abbreviation + "_tr" + str(training_size) + "_te" + str(test_size) + "_runs" + str(number_runs) + ".txt", np.c_[run,r_sq], fmt='%i	%f')
       
-    return modopt,modtest
+    return self.modopt,self.modtest
 
   def bayes_opt2(self,training_size, test_size, ker, acquisition_function, noise=0.01, number_runs=None, end_train=None, print_interim=False):
     
@@ -135,10 +146,10 @@ class Experiment(object):
         print "Iteration",i,"..."
         
       # RUN OPTIMISATION FUNCTION TO FIND THE NEW INPUT VALUE, AUTOMATICALLY ADDED TO THE TRAINING SET AND REMOVED FROM THE OPTIMISATION SET
-      modopt.hyperparameters(print_vals=False) #######################?????????????????????
+ #######################?????????????????????
       print modopt.kernel.kers[0].sig_var, modopt.kernel.kers[0].lengthscale, modopt.kernel.kers[1].sig_var, modopt.kernel.kers[1].lengthscale ### at i=0 this should be the same as modtest hparams above
       newx = modopt.optimization()
-      
+
       # RESET TRAINING SET OF THE TEST SET MODEL
       modtest.Xtrain = modopt.Xtrain
       modtest.Ytrain = modopt.Ytrain
